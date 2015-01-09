c9d03e1d378033059ed6d393cd49bef509035b7c
Bob <bcolner@gmail.com>
2015-01-08 20:06:39 -0800

    added flask_debugtoolbar

--- manage.py
+++ manage.py
@@ -17,11 +17,12 @@ from app import create_app, db
 from app.models import User, Follow, Role, Permission, Post, Comment
 from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app, db)
-
+toolbar = DebugToolbarExtension(app)
 
 def make_shell_context():
     return dict(app=app, db=db, User=User, Follow=Follow, Role=Role,
