5ec6482105ff8e0acf02b054908265d75cbe18f3
Bob <bcolner@gmail.com>
2015-01-09 14:31:12 -0800

    experimenting with app/static/public static file serving. Does not look like a good approach.

--- app/static/Gruntfile.js
+++ app/static/Gruntfile.js
@@ -1,25 +1,26 @@
 // Gruntfile.js
-module.exports = function (grunt) {
-  grunt.initConfig({
-    // Watch task config
-    watch: {
-      sass: {
-        files: "sass/*.scss",
-        files: "sass/*.sass",
-        tasks: ['sass']
-      }
-    },
-    // SASS task config
-    sass: {
-        dev: {
-            files: {
-                // destination        // source file
-                "public/styles.css" : "sass/material_styles.scss"
+module.exports = function(grunt) {
+    grunt.initConfig({
+        // Watch task config
+        watch: {
+            sass: {
+                files: "sass/*.scss",
+                files: "sass/*.sass",
+                tasks: ['sass']
             }
-        }
-    },
-  });
+        },
+        // SASS task config
+        sass: {
+            dev: {
+                files: {
+                    // destination        // source file
+                    "public/css/styles.css": "sass/material_styles.scss",
+                    "public/css/materialize.css": "bower_components/materialize/sass/materialize.scss"
+                }
+            }
+        },
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 };
