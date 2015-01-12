92ab42870c72caa6757268a2ca11670eb04417e5
Bob <bcolner@gmail.com>
2015-01-12 02:47:40 -0800

    achieved holy grail layout. last fix was by chance, need to understand why;)

--- app/matgirl/views.py
+++ app/matgirl/views.py
@@ -1,9 +1,11 @@
-from flask import render_template, redirect, request, url_for, flash
+from flask import render_template, redirect, request, url_for, flash, jsonify
 from flask.ext.login import login_user, logout_user, login_required, current_user
 from . import matgirl
 from .. import db
 from .forms import WelcomeForm, LoginForm, RegistrationForm
 
+@matgirl.route('/')
+@matgirl.route('/home')
 @matgirl.route('/welcome')
 def welcome():
     form = WelcomeForm()
@@ -13,6 +15,19 @@ def welcome():
 def login():
     form = LoginForm()
     return render_template('matgirl/login.html', form=form)
+
+@matgirl.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('matgirl/register.html', form=form)
+
+@matgirl.route('/layout')
+def layout():
+	return render_template('matgirl/layout.html')
+
+@matgirl.route('/cards')
+def cards():
+    return render_template('matgirl/cards.html')    
 
 @matgirl.route('/login/real', methods=['GET', 'POST'])
 def login_real():
@@ -25,21 +40,9 @@ def login_real():
         flash('Invalid username or password.')
     return render_template('matgirl/login.html', form=form)
 
-
-@matgirl.route('/register')
-def register():
-    form = RegistrationForm()
-    return render_template('matgirl/register.html', form=form)
-
 @matgirl.route('/_add_numbers')
 def add_numbers():
     """Add two numbers server side, ridiculous but well..."""
     a = request.args.get('a', 0, type=int)
     b = request.args.get('b', 0, type=int)
     return jsonify(result=a + b)
-
-
-@matgirl.route('/')
-def index():
-    return render_template('index.html')
-
